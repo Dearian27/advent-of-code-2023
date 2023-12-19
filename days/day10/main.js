e2593d8dbd28b366c02c4c6a1f79821dad0cf17d
dearian <dearian2706@gmail.com>
2023-12-19 23:47:19 +0200

    day10

--- days/day10/main.js
+++ days/day10/main.js
@@ -1,7 +1,7 @@
 const cellsize = 8;
 
 const values = {
-  'S': 'partF',
+  'S': 'part7',
 
   'F': 'partF',
   'L': 'partL',
@@ -13,7 +13,16 @@ const values = {
 }
 const valids = ['F', 'L', '7', 'J', '|', '-', 'S']
 const ways = {
+  // [y, x]
+  'S': [[0, -1], [1, 0]],
 
+  'F': [[0, 1], [1, 0]],
+  'L': [[-1, 0], [0, 1]],
+  '7': [[0, -1], [1, 0]],
+  'J': [[-1, 0], [0, -1]],
+  '-': [[0, -1], [0, 1]],
+  '|': [[-1, 0], [1, 0]],
+  '.': [],
 }
 
 const canvas = document.getElementById('canvas');
@@ -21,10 +30,13 @@ canvas.style.width = inputs[0].length * cellsize + 'px';
 canvas.style.height = inputs.length * cellsize + 'px'; 
 
 const cells = [];
+const loop = [];
+let startPoint;
 
 const createCell = (value, x, y) => {
   let part;
   switch(value) {
+    case 'S': 
     case 'F': 
     case 'L': 
     case '7': 
@@ -35,9 +47,9 @@ const createCell = (value, x, y) => {
   }
   const cell = new DOMParser().parseFromString(part, 'image/svg+xml').querySelector('svg');
   const path = cell.querySelector('.path');
-  if(path) path.style.fill = getPixelColor(pixelIndex, 0);
-  // if(path) path.style.fill = 'grey';
-  // if(path && value === 'S') path.style.fill = 'red';
+  // if(path) path.style.fill = getPixelColor(pixelIndex, 0);
+  if(path) path.style.fill = 'grey';
+  if(path && value === 'S') path.style.fill = 'red';
   cell.classList.add('cell');  
   cell.style.top = y * cellsize + 'px';
   cell.style.left = x * cellsize + 'px';
@@ -46,23 +58,65 @@ const createCell = (value, x, y) => {
   if(values[value]) {
     cell.classList.add(values[value]);
   }
-  return {cell, path, value};
+  if(value === 'S') {
+    startPoint = {cell, path, value, position: {x, y}}
+  }
+  return {cell, path, value, position: {x, y}};
 }
 
 inputs.map((row, y) => {
+  cells[y] = [];
   row.map((cell, x) => {
-    const {cell: cellHTML} = createCell(cell, x, y);
+    const {cell: cellHTML, path, value, position} = createCell(cell, x, y);
     canvas.appendChild(cellHTML);
+    cells[y].push({cell: cellHTML, path, value, position});
     pixelIndex = (pixelIndex + 1) % rainbowCanvas.width;
   })
 })
 
+console.log(cells)
 
+let looped = false;
+let lastWays = [];
+let activeWays = [startPoint];
+let loopLength = 0;
 
-
-function checkAll () {
-
-  setTimeout(() => {
-    checkAll();
-  }, 200)
+const removeWay = (way) => {
+  activeWays = activeWays.filter(w => w !== way);
 }
+
+function checkAll() {
+  const waysToAdd = [];
+  let finish = true;
+  activeWays.forEach(way => {
+    ways[way.value].forEach(adds => {
+      const current = cells[way.position.y + adds[0]][way.position.x + adds[1]];
+      let isLast = false;
+      lastWays.forEach(last => {
+        if(current === last) isLast = true; 
+      })
+      if(current && !isLast) {
+        waysToAdd.push(current);
+        finish = false;
+        current.cell.querySelector('path').style.fill = 'red'
+      }
+    })
+  })
+  lastWays = activeWays;
+  activeWays = waysToAdd;
+  if(finish) {
+    looped = true;
+  } else loopLength++;
+  console.log(loopLength)
+  if(!looped) {
+    // setTimeout(() => {
+      checkAll();
+    // }, 2)
+  }
+}
+
+// document.onload = () => {
+//   checkAll();
+// }
+
+checkAll();
